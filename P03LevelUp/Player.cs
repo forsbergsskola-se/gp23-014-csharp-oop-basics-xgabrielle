b4be603aa8731d534c5e1d09edb9be484e15a16f
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2023-10-10 22:16:57 +0200

    p3 so close, yet so far ..

---
+++ P03LevelUp/Player.cs
@@ -0,0 +1,22 @@
+﻿public class Player
+{
+    public void GrantExperience(int level, int experience)
+    {
+        Console.WriteLine($"{level} Level and {experience} Experience");
+        while (true)
+        {
+            experience = int.Parse(Console.ReadLine());
+            if (experience > 100)
+            {
+                level++;
+                experience = experience - 100;
+                Console.WriteLine($"{level} Level and {experience} Experience");
+            }
+            else
+            {
+                Console.WriteLine($"{level} Level and {experience} Experience");
+            }
+            
+        }
+    }
+}
---
+++ P03LevelUp/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+Player grant = new Player();
+
+grant.GrantExperience(0,0);
+grant.GrantExperience(0,0);
+grant.GrantExperience(0,0);
+grant.GrantExperience(0,0);
+
+
+
+
