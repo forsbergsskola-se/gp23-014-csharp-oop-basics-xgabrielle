660091f28581e7c5ddd212ce35f39f8c93098a16
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2023-10-11 13:41:44 +0200

    p4_1 done

---
+++ P4_1Replacing/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+Plant plant = new Plant();
+
+plant.PrintStatus();
+// FUCKING GROW!!!!!!!!!!!
+plant.Grow();
+// FUCKING GROW!!!!!!!!
+plant.PrintStatus();
+plant.Grow();
+plant.PrintStatus();
+
