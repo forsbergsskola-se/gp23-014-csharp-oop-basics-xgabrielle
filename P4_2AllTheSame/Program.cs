fe85f6aec860bf3355968cdeba79821eb4147618
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2023-10-11 14:38:34 +0200

    p4_2 done

---
+++ P4_2AllTheSame/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+ScoreCounter one = new ScoreCounter();
+ScoreCounter two = one;
+ScoreCounter three = two;
+ScoreCounter four = three;
+
+one.IncreaseScore();
+two.IncreaseScore();
+three.IncreaseScore();
+four.IncreaseScore();
+
+Console.WriteLine(one.score);
+Console.WriteLine(two.score);
+Console.WriteLine(three.score);
+Console.WriteLine(four.score);
