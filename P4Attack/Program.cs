41c0eac97907cd6f886e6550dd9ba94af4958a73
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2023-10-11 23:20:50 +0200

    p4 done ish
    confused about the description..

---
+++ P4Attack/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Threading.Channels;
+
+Console.WriteLine("Hello, World!");
+
+Player playerOne = new Player();
+Player playerTwo = new Player();
+Player playerThree = playerOne;
+
+Console.WriteLine("Player 1 attacks Player 2");
+playerOne.Attack(playerTwo);
+Console.WriteLine($"Player 1, Damage: {playerOne.damage}");
+Console.WriteLine($"Player 2, Damage: {playerTwo.damage}");
+
+Console.WriteLine("Player 2 attacks Player 1");
+playerTwo.Attack(playerOne);
+Console.WriteLine($"Player 1, Damage: {playerOne.damage}");
+Console.WriteLine($"Player 2, Damage: {playerTwo.damage}");
+
+Console.WriteLine("Player 1 attacks Player 2");
+playerOne.Attack(playerTwo);
+Console.WriteLine($"Player 1, Damage: {playerOne.damage}");
+Console.WriteLine($"Player 2, Damage: {playerTwo.damage}");
+
+Console.WriteLine("Player 2 attacks Player 3");
+playerTwo.Attack(playerThree);
+Console.WriteLine($"Player 1, Damage: {playerOne.damage}");
+Console.WriteLine($"Player 2, Damage: {playerTwo.damage}");
+Console.WriteLine($"Player 3, Damage: {playerThree.damage}");
