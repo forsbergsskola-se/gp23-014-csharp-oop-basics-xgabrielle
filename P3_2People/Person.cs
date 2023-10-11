8e8be36d049fa5b5b4d19d28f0fef0779b51f674
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2023-10-11 09:54:07 +0200

    p3_2 done

---
+++ P3_2People/Person.cs
@@ -0,0 +1,10 @@
+﻿
+public class Person
+{
+    public string name;
+    
+    public void Greeting()
+    {
+        Console.WriteLine($"Hello I'm {name}");
+    }
+}
---
+++ P3_2People/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Person[] people = new Person [3];
+
+Console.WriteLine("Give me names");
+for (var i = 0; i < people.Length; i++)
+{
+    people[i] = new Person();
+}
+
+people[0].name=(Console.ReadLine());
+people[1].name=(Console.ReadLine());
+people[2].name=(Console.ReadLine());
+
+for (var i = 0; i < people.Length; i++)
+{
+           people[i].Greeting(); 
+}
