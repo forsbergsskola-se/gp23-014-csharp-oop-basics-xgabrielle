8e48bc18088b5fe0524ab5d25c313f2072e8dfa4
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2023-10-13 21:10:11 +0200

    p4 done

--- P4Attack/Program.cs
+++ P4Attack/Program.cs
@@ -6,7 +6,7 @@ Console.WriteLine("Hello, World!");
 
 Player playerOne = new Player();
 Player playerTwo = new Player();
-Player playerThree = playerOne;
+
 
 Console.WriteLine("Player 1 attacks Player 2");
 playerOne.Attack(playerTwo);
@@ -18,13 +18,15 @@ playerTwo.Attack(playerOne);
 Console.WriteLine($"Player 1, Damage: {playerOne.damage}");
 Console.WriteLine($"Player 2, Damage: {playerTwo.damage}");
 
-Console.WriteLine("Player 1 attacks Player 2");
-playerOne.Attack(playerTwo);
+Console.WriteLine("Player 1 attacks Player 1");
+playerOne.Attack(playerOne);
 Console.WriteLine($"Player 1, Damage: {playerOne.damage}");
 Console.WriteLine($"Player 2, Damage: {playerTwo.damage}");
 
-Console.WriteLine("Player 2 attacks Player 3");
-playerTwo.Attack(playerThree);
+Console.WriteLine("Player 2 attacks Player 1");
+playerTwo.Attack(playerOne);
 Console.WriteLine($"Player 1, Damage: {playerOne.damage}");
 Console.WriteLine($"Player 2, Damage: {playerTwo.damage}");
+
+Player playerThree = playerOne;
 Console.WriteLine($"Player 3, Damage: {playerThree.damage}");
