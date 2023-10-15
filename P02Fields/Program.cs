72ea514188340d4e365bb08dc0ebb4f324bd7661
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2023-10-15 10:19:42 +0200

    p02 done

--- P02Fields/Program.cs
+++ P02Fields/Program.cs
@@ -10,3 +10,14 @@
 
 Console.WriteLine();
 
+Player player = new Player();
+
+Console.WriteLine(player.name);
+Console.WriteLine(player.email);
+Console.WriteLine(player.experience);
+Console.WriteLine(player.gold);
+Console.WriteLine(player.pvp);
+Console.WriteLine(player.vip);
+Console.WriteLine(player.level);
+Console.WriteLine(player.stone);
+Console.WriteLine(player.wood);
