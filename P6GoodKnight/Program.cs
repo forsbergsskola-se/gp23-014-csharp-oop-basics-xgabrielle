572c2c0ee7c4e4f7f39bf3400e8246e00fec1941
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2023-11-03 13:22:17 +0100

    P6 done

---
+++ P6GoodKnight/Program.cs
@@ -0,0 +1,21 @@
+﻿
+Knight attacker = new Knight();
+Knight defender = new Knight();
+
+
+defender.shield = new Shield();
+attacker.Attack(defender);
+attacker.sword = new Sword();
+Console.WriteLine("Equipping Sword...");
+attacker.Attack(defender);
+attacker.Attack(defender);
+
+
+
+
+
+
+
+
+
+
